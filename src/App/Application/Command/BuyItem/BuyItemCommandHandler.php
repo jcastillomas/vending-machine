9ee49ba21525620e392e0296198d8c68269b449c
Jordi Castillo <jordi.castillo@promocionesfarma.com>
2025-11-29 12:10:48 +0100

    feat(app): describe in handler of buy-item the execution flow

--- src/App/Application/Command/BuyItem/BuyItemCommandHandler.php
+++ src/App/Application/Command/BuyItem/BuyItemCommandHandler.php
@@ -13,6 +13,36 @@ final readonly class BuyItemCommandHandler implements CommandHandlerInterface
     ) {
     }
 
+    /*
+        TODO: @Buy item use case:
+            1. Find Product by name
+            2. Find Currencies
+            3. Find Fund
+            4. Check fund amount and product value
+            5. Calculate change
+            6. Find Cash
+            7. Calculate change in Cash
+            8. Update Stock
+            9. Update Cash
+           10. Reset Fund
+           11. Return Response
+     */
+    /**
+     * Execution process:
+     *  1. Find Product by name
+     *  2. Find Currencies
+     *  3. Find Fund
+     *  4. Check fund amount and product value
+     *  5. Calculate change
+     *  6. Find Cash
+     *  7. Calculate change in Cash
+     *  8. Update Stock
+     *  9. Update Cash
+     * 10. Reset Fund
+     * 11. Return Response
+     * @param BuyItemCommand $command
+     * @return BuyItemCommandResponse
+     */
     public function __invoke(BuyItemCommand $command): BuyItemCommandResponse
     {
         return $this->responseConverter->__invoke("WATER", ["0.25", "0.10"]);
